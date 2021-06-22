5bb12f3f3fe125c62537f5335afd1fd4d99287f9
dirty_hippy <piaiws@gmail.com>
2021-06-22 13:41:03 +0300

    fix brain_prime tests

--- brain_games/scripts/brain_prime.py
+++ brain_games/scripts/brain_prime.py
@@ -27,11 +27,9 @@ def make_question():
 
 
 def is_prime(n):
-    if n % 2 == 0:
-        return n == 2
-    d = 3
+    d = 2
     while d * d <= n and n % d != 0:
-        d += 2
+        d += 1
     return 'yes' if d * d > n else 'no'
 
 
