5486af140f888e56be173dc31af334d3281842de
dirty_hippy <piaiws@gmail.com>
2021-07-07 14:34:42 +0300

    added operator

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -1,22 +1,15 @@
 import random
+import operator
 
 
 GAME_RULES = 'What is the result of the expression?'
+OPERATIONS = {'+': operator.add, '-': operator.sub, '*': operator.mul}
 
 
 def make_question():
     first_number = random.randint(1, 10)
     second_number = random.randint(1, 10)
-    operation = random.choice('+-*')
+    operation = random.choice(list(OPERATIONS.keys()))
     question = "{} {} {}".format(first_number, operation, second_number)
-    correct_answer = calc(first_number, second_number, operation)
+    correct_answer = OPERATIONS[operation](first_number, second_number)
     return question, str(correct_answer)
-
-
-def calc(first_number, second_number, operation):
-    if operation == '+':
-        return first_number + second_number
-    elif operation == '-':
-        return first_number - second_number
-    elif operation == '*':
-        return first_number * second_number
