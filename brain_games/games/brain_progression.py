b4aefe50474aafda0841cda3e907c8307cc145af
dirty_hippy <piaiws@gmail.com>
2021-07-07 15:45:02 +0300

    change generation progerssion

--- brain_games/games/brain_progression.py
+++ brain_games/games/brain_progression.py
@@ -7,10 +7,7 @@ GAME_RULES = 'What number is missing in the progression?'
 def make_question():
     first_number = random.randint(1, 100)
     step = random.randint(1, 10)
-    progression = []
-    for i in range(10):
-        next_number = first_number + i * step
-        progression.append(str(next_number))
+    progression = [str(first_number + i * step) for i in range(10)]
     secret_number_index = random.randint(0, len(progression) - 1)
     secret_number = progression[secret_number_index]
     progression[secret_number_index] = '..'
