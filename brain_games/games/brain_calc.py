6220eed508f8bb921d552415ca619e4713b406da
dirty_hippy <piaiws@gmail.com>
2021-07-07 15:47:24 +0300

    delete global operations

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -3,13 +3,13 @@ import operator
 
 
 GAME_RULES = 'What is the result of the expression?'
-OPERATIONS = {'+': operator.add, '-': operator.sub, '*': operator.mul}
 
 
 def make_question():
+    operations = {'+': operator.add, '-': operator.sub, '*': operator.mul}
     first_number = random.randint(1, 10)
     second_number = random.randint(1, 10)
-    operation = random.choice(list(OPERATIONS.keys()))
+    operation = random.choice(list(operations.keys()))
     question = "{} {} {}".format(first_number, operation, second_number)
-    correct_answer = OPERATIONS[operation](first_number, second_number)
+    correct_answer = operations[operation](first_number, second_number)
     return question, str(correct_answer)
